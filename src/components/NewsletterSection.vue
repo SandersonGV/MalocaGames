13a63f5b498c05c8b4a7abe5c31d9cf5a3a4b147
Sanderson <virgolinos@gmail.com>
2023-03-17 01:33:45 -0300

    disabilitar botao

--- src/components/NewsletterSection.vue
+++ src/components/NewsletterSection.vue
@@ -8,33 +8,37 @@ export default {
   },
   data() {
     return {
-      newsletterController : new NewsletterController(),
-      email : "",
-      toast:{}
+        enviando:false,
+        newsletterController : new NewsletterController(),
+        email : "",
+        toast:{}
     };
   },
   methods: {
     async enviarInscrito(){
+        this.enviando = true;
         if(!this.validarEmail(this.email)){
-            this.toast.fire({
+            await this.toast.fire({
                 icon: 'error',
                 title: 'Preencha um email válido'
             })
+            this.enviando = false;
             return; 
         }
         let result = await this.newsletterController.addEmail(this.email);
 
         if(!result){
-            this.toast.fire({
+            await this.toast.fire({
                 icon: 'warning',
                 title: 'ocorreu um erro no servidor, tente novamente mais tarde'
             })
         }
 
-        this.toast.fire({
+        await this.toast.fire({
             icon: 'success',
             title: 'Email registrado com sucesso'
         })
+        this.enviando = false;
     },
     validarEmail(email) {
         const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
@@ -47,7 +51,7 @@ export default {
         toast: true,
         position: 'top-end',
         showConfirmButton: false,
-        timer: 3000,
+        timer: 1300,
         timerProgressBar: true,
         didOpen: (toast) => {
             toast.addEventListener('mouseenter', Swal.stopTimer)
@@ -68,7 +72,7 @@ export default {
                             <h4 class="mb-4">Se inscreva para novidades <span class="text-primary text-uppercase">novidades</span></h4>
                             <div class="position-relative mx-auto" style="max-width: 400px;">
                                 <input class="form-control w-100 py-3 ps-4 pe-5" type="text" v-model="email" placeholder="Seu email aqui...">
-                                <button type="button" class="btn btn-primary py-2 px-3 position-absolute top-0 end-0 mt-2 me-2" @click="enviarInscrito">Submit</button>
+                                <button :disabled="enviando" type="button" class="btn btn-primary py-2 px-3 position-absolute top-0 end-0 mt-2 me-2" @click="enviarInscrito">Submit</button>
                             </div>
                         </div>
                     </div>
